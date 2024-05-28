c5073280aea446ed735240d0b79b90e5e54093bd
Nitish-op <nitish.araja@gmail.com>
2024-05-28 15:10:06 +0530

    course-enroll

--- src/app/dashboard/courses/course-enroll/[courseId]/page.tsx
+++ src/app/dashboard/courses/course-enroll/[courseId]/page.tsx
@@ -2,38 +2,38 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'next/navigation';
 import axios from 'axios';
-import { courses } from '../../course-constants';
+import { courses as staticCourse } from '../../course-constants';
 import CourseEnroll from '@/components/dashboard-components/courses-components/course-enroll/course-enroll';
 
 const CourseEnrollPage = () => {
   const params = useParams();
   const courseId = params.courseId;
-  const [course, setCourse] = useState<any>(null);
+  const [course, setCourse] = useState<any>(staticCourse[1]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
 
-  useEffect(() => {
-    const fetchCourse = async () => {
-      try {
-        const response = await axios.get(`http://localhost:4500/courses/${courseId}`);
-        setCourse(response.data);
-        setLoading(false);
-      } catch (err) {
-        setError('Course not found');
-        setLoading(false);
-      }
-    };
+  // useEffect(() => {
+  //   const fetchCourse = async () => {
+  //     try {
+  //       const response = await axios.get(`http://localhost:4500/courses/${courseId}`);
+  //       setCourse(response.data);
+  //       setLoading(false);
+  //     } catch (err) {
+  //       setError('Course not found');
+  //       setLoading(false);
+  //     }
+  //   };
 
-    fetchCourse();
-  }, [courseId]);
+  //   fetchCourse();
+  // }, [courseId]);
 
-  if (loading) {
-    return <div>Loading...</div>;
-  }
+  // if (loading) {
+  //   return <div>Loading...</div>;
+  // }
 
-  if (error || !course) { // Check if course is null
-    return <div>{error || 'Course not found'}</div>;
-  }
+  // if (error || !course) { // Check if course is null
+  //   return <div>{error || 'Course not found'}</div>;
+  // }
 
   return <CourseEnroll course={course} />;
 };
