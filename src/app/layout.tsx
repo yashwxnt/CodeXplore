0c40e1fb67eaf40b1bae5bba93044eb736ad136b
yashwxnt <yashwanthjawaji@gmail.com>
2024-05-30 10:18:18 +0530

    new course-enroll

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter} from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/ui/themeprovider";
+import { Toaster } from "@/components/ui/toaster";
 
 const inter = Inter({
   weight: ["400"],
@@ -28,6 +29,7 @@ export default function RootLayout({
         disableTransitionOnChange
       >
         {children}
+        <Toaster/>
       </ThemeProvider>
     </body>
   </html>
