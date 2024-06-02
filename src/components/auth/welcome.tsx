279cc1b199790f98e1a4b3d2961fc133538a9621
TalkingTeddy <faisalilyas39@gmail.com>
2024-06-02 15:53:39 +0530

    Something

--- src/components/auth/welcome.tsx
+++ src/components/auth/welcome.tsx
@@ -4,12 +4,15 @@ import { Card, CardDescription, CardHeader, CardTitle } from "../ui/card"
 import '../ui/TypeWriter.css'
 
 const Welcome = () => {
+
+    const textReveal = "The ultimate gamified platform for coding and education! Dive into interactive challenges, explore coding puzzles, and unlock achievements as you enhance your programming skills. Whether you're a beginner or an experienced coder, CodeXplore offers a fun and engaging way to learn and grow."
+
     return (
         <Card className={`w-1/2 h-2/4 flex flex-col justify-center items-center bg-transparent border-none text-white`}>
             <CardTitle className={`p-3 font-brenet-outline`}>
                 Welcome to CodeExplore
             </CardTitle>
-            <CardDescription className={`text-center font-brenet-regular text-violet-400 overflow-hidden border-r-2 border-orange-500 mx-auto tracking-tighter animate-typing`}>
+            <CardDescription className={`text-center font-brenet-regular text-violet-400 overflow-hidden border-r-2 border-orange-500 mx-auto animate-typing`}>
                 The ultimate gamified platform for coding and education! Dive into interactive challenges, explore coding puzzles, and unlock achievements as you enhance your programming skills. Whether you're a beginner or an experienced coder, CodeXplore offers a fun and engaging way to learn and grow.
             </CardDescription>
         </Card>
